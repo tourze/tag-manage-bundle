ea830a039da351673713a4b6a17d8dd33c496df9
github-ci <you@example.com>
2025-11-18 23:23:49 +0800

    fix error

--- tests/TagManageBundleTest.php
+++ tests/TagManageBundleTest.php
@@ -2,18 +2,8 @@
 
 namespace Tourze\TagManageBundle\Tests;
 
-use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
-use Knp\Bundle\PaginatorBundle\KnpPaginatorBundle;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\RunTestsInSeparateProcesses;
-use Tourze\BundleDependency\BundleDependencyInterface;
-use Tourze\DoctrineIndexedBundle\DoctrineIndexedBundle;
-use Tourze\DoctrineIpBundle\DoctrineIpBundle;
-use Tourze\DoctrineTimestampBundle\DoctrineTimestampBundle;
-use Tourze\DoctrineTrackBundle\DoctrineTrackBundle;
-use Tourze\DoctrineUserBundle\DoctrineUserBundle;
-use Tourze\EasyAdminMenuBundle\EasyAdminMenuBundle;
-use Tourze\JsonRPCPaginatorBundle\JsonRPCPaginatorBundle;
 use Tourze\PHPUnitSymfonyKernelTest\AbstractBundleTestCase;
 use Tourze\TagManageBundle\TagManageBundle;
 
@@ -24,22 +14,4 @@ use Tourze\TagManageBundle\TagManageBundle;
 #[RunTestsInSeparateProcesses]
 final class TagManageBundleTest extends AbstractBundleTestCase
 {
-    public function testGetBundleDependencies(): void
-    {
-        $dependencies = TagManageBundle::getBundleDependencies();
-
-        $expectedDependencies = [
-            DoctrineBundle::class => ['all' => true],
-            KnpPaginatorBundle::class => ['all' => true],
-            JsonRPCPaginatorBundle::class => ['all' => true],
-            DoctrineIndexedBundle::class => ['all' => true],
-            DoctrineIpBundle::class => ['all' => true],
-            DoctrineTrackBundle::class => ['all' => true],
-            DoctrineTimestampBundle::class => ['all' => true],
-            DoctrineUserBundle::class => ['all' => true],
-            EasyAdminMenuBundle::class => ['all' => true],
-        ];
-
-        $this->assertSame($expectedDependencies, $dependencies);
-    }
 }
